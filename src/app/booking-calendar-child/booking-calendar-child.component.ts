24f13736f8c76b81d3964b632f38880335456ed4
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-10-16 10:28:42 +0200

    he traducido la cabecera del calendario a cast, inglés y, parte, català

--- src/app/booking-calendar-child/booking-calendar-child.component.ts
+++ src/app/booking-calendar-child/booking-calendar-child.component.ts
@@ -217,23 +217,7 @@ export class BookingCalendarChildComponent {
   }
 
   ngOnInit() {
-
-
-      switch ( localStorage.getItem('preferredLang') ) {
-        case 'ca-ES':
-          this.currentLang = 'ca-ES'
-        break
-        case 'es-ES':
-          this.currentLang = 'es-ES'      
-        break
-        case 'en-EN':
-          this.currentLang = 'en-EN'
-        break
-        default:
-          this.currentLang = 'ca-ES'
-      }
-
-
+    this.currentLang = localStorage.getItem('preferredLang')
     this.loadBookingList()
   }
 
